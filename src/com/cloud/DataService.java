1f97ec74f403b1e5967dc0c7915db79eaab0b2ce
Shubham Goel <shubhamgoel7703@gmail.com>
2018-01-17 18:54:47 +0530

    service file modified

--- src/com/cloud/DataService.java
+++ src/com/cloud/DataService.java
@@ -1,6 +1,12 @@
 package com.cloud;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +35,7 @@ public class DataService
 		   System.out.println("coming in java");
 		   System.out.println(name);
 		   
-		
+		   HttpRequest(name);
 		   
 	      return "Shubham";//userDao.getAllUsers();
 	   }
@@ -47,5 +53,52 @@ public class DataService
 		
 	   }
 	 
+	   
+	   public void HttpRequest(String browserUrl)
+	   {
+		   HttpURLConnection connection = null;
+		   String targetURL = browserUrl+"rest/PostService/restPost";
+		   String urlParameters="";
+		   try {
+			    //Create connection
+			    URL url = new URL(targetURL);
+			    connection = (HttpURLConnection) url.openConnection();
+			    connection.setRequestMethod("POST");
+			    connection.setRequestProperty("Content-Type", 
+			        "application/x-www-form-urlencoded");
+
+			    connection.setRequestProperty("Content-Length", 
+			        Integer.toString(urlParameters.getBytes().length));
+			    connection.setRequestProperty("Content-Language", "en-US");  
+
+			    connection.setUseCaches(false);
+			    connection.setDoOutput(true);
+
+			    //Send request
+			    DataOutputStream wr = new DataOutputStream (
+			        connection.getOutputStream());
+			    wr.writeBytes(urlParameters);
+			    wr.close();
+
+			    //Get Response  
+			    InputStream is = connection.getInputStream();
+			    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+			    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+			    String line;
+			    while ((line = rd.readLine()) != null) {
+			      response.append(line);
+			      response.append('\r');
+			    }
+			    rd.close();
+			    System.out.println( response.toString());
+			  } catch (Exception e) {
+			    e.printStackTrace();
+			  
+			  } finally {
+			    if (connection != null) {
+			      connection.disconnect();
+			    }
+			  }
+	   }
 	
 }
